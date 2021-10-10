9183dea5991077de3f4c532b6e5f91d17f2c49a5
kadamyash <yash11398@gmail.com>
2021-10-10 16:24:31 +0530

    framework

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+if(localStorage.getItem('theme')!==null){
+    const theme = localStorage.getItem('theme');
+    document.getElementById('main').className = theme;
+    if(theme==='light'){
+            document.getElementById('theme-container').innerHTML="Switch to dark";
+    }
+    else if(theme==='dark'){
+            document.getElementById('theme-container').innerHTML="Switch to light";
+    }
+}
+else{
+    document.getElementById('main').className = 'light';
+}
+
+function switchTheme(){
+    const element = document.getElementById('main');
+    if(element.className==='light'){
+        element.className='dark';
+        localStorage.setItem('theme', 'dark');
+        document.getElementById('theme-container').innerHTML="Switch to light";
+    }
+    else{
+        element.className='light';
+        localStorage.setItem('theme', 'light');
+        document.getElementById('theme-container').innerHTML="Switch to dark";
+    }
+}
